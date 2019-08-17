584937993eb1f1c7854f0ccee0648d0f30098619
Wazarr94 <39807740+Wazarr94@users.noreply.github.com>
2019-08-18 00:01:43 +0200

    rename and style changes

--- Level_0/HaxBot_skeleton.js
+++ Level_0/HaxBot_skeleton.js
@@ -2,24 +2,18 @@
 
 /* ROOM */
 
-const roomName = "Room Name";
+const roomName = "Hello World";
 const botName = "HaxBot";
 const maxPlayers = 12;
 const roomPublic = true;
-const geo = [{"code": "DE", "lat": 51.1, "lon": 10.4}, {"code": "FR", "lat": 46.2, "lon": 2.2}, {"code": "PL", "lat": 51.9, "lon": 19.1}, {"code": "GB", "lat": 55.3, "lon": -3.4}, {"code": "PT", "lat": 39.3, "lon": -8.2}];
 const scoreLimit = 3;
 const timeLimit = 3;
-const Team = {
-	SPECTATORS: 0,
-	RED: 1,
-	BLUE: 2
-};
+const Team = { SPECTATORS: 0, RED: 1, BLUE: 2 };
 
-const room = HBInit({roomName: roomName, maxPlayers: maxPlayers, public: roomPublic, playerName: botName, geo: geo[0]});
+const room = HBInit({ roomName: roomName, maxPlayers: maxPlayers, public: roomPublic, playerName: botName });
 
 room.setScoreLimit(scoreLimit);
 room.setTimeLimit(timeLimit);
-room.setTeamsLock(true);
 
 /* EVENTS */
 
