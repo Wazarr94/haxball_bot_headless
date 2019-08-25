a1915edd9dee552f9efe675403fe5564ea3fd4d7
Wazarr94 <39807740+Wazarr94@users.noreply.github.com>
2019-08-25 17:17:04 +0200

    Update adminHandle.js

--- Level_1/adminHandle.js
+++ Level_1/adminHandle.js
@@ -2,57 +2,69 @@
 
 2 things in this file : the updateAdmins function which allow to keep an admin anytime, and the adminPassword.
 First thing is much wanted if you plan to have your room going while you're away.
-The second one makes it so if you accidentaly lose your admin, you can recover it. The password will show up in the console.
+The second one makes it so if you accidentally lose your admin, you can recover it. The password will show up in the console.
+
+1.1 UPDATE :
+When an admin unadmins himself while he's the only admin, he's not going to get admin back immidiately.
+Also fixed the possibility there are no admins in the room in case the only admin unadmins himself. 
 
 */
 
 /* ROOM */
 
+var players;
+
 var adminPassword = 1000 + getRandomInt(9000);
-console.log("adminPassword : " + adminPassword);
+console.log(`adminPassword : ${adminPassword}`);
 
 /* FUNCTIONS */
 
 /* AUXILIARY FUNCTIONS */
 
-function getRandomInt(max) { // Returns a random number from 0 to max-1
+function getRandomInt(max) { // returns a random number from 0 to max-1
 	return Math.floor(Math.random() * Math.floor(max)); 
 }
 
 function arrayMin(arr) {
-	var len = arr.length, min = Infinity;
+	var len = arr.length;
+	var min = Infinity;
 	while (len--) {
-		if (arr[len] < min) {
-			min = arr[len];
-	  	}
+		if (arr[len] < min) min = arr[len];
 	}
 	return min;
 }
 
 /* PLAYER FUNCTIONS */
 
-function updateAdmins() {
-	if (players.length == 0 || players.find((player) => player.admin) != null) {
-		return;
+function updateAdmins(excludedPlayerID = null) {
+	if (players.length != 0 && players.find((p) => p.admin) == null) {
+		if (excludedPlayerID != null) {
+			let playerArray = players.filter((p) => p.id != excludedPlayerID);
+			let arrayID = playerArray.map((player) => player.id);
+			room.setPlayerAdmin(arrayMin(arrayID), true);
+		}
+		else {
+			let arrayID = players.map((player) => player.id);
+			room.setPlayerAdmin(arrayMin(arrayID), true);
+		}
 	}
-	var copie = []; 
-	players.forEach(function(element){ copie.push(element.id); });
-	room.setPlayerAdmin(arrayMin(copie), true); // Give admin to the player who's played the longest on the room
 }
 
 /* EVENTS */
 
 room.onPlayerJoin = function(player) {
+	players = room.getPlayerList().filter((p) => p.id != 0);
 	updateAdmins();
 }
 
 room.onPlayerLeave = function(player) {
+	players = room.getPlayerList().filter((p) => p.id != 0);
 	updateAdmins();
 }
 
 room.onPlayerChat = function(player, message) {
-	message = message.split(" ");
-	if (message[0] == '!claim') {
+	message = message.split(/ +/);
+	if (["!claim"].includes(message[0].toLowerCase())) {
 		if (message[1] == adminPassword) {
 			room.setPlayerAdmin(player.id, true);
 		}
@@ -60,4 +72,8 @@ room.onPlayerChat = function(player, message) {
 	if (message[0][0] == "!") { // Make sure this condition stays at the bottom of this function
 		return false;
 	}
+}
+
+room.onPlayerAdminChange = function(changedPlayer, byPlayer) {
+	updateAdmins(changedPlayer.admin == false && changedPlayer.id == byPlayer.id ? changedPlayer.id : null);
 }
